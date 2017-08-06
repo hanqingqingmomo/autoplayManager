7832987e8033ed9d3b1df89fc9b24b137664d2ac
hanqing <qing.han@bigmarker.com>
2017-08-06 20:05:31 +0800

    add emit event

--- app/models/autoplay.model.js
+++ app/models/autoplay.model.js
@@ -3,20 +3,21 @@ Schema = mongoose.Schema;
 
 var Autoplay = new Schema({
    conferenceId: String,
+   serverUrl: String,
+   keys: String,
    link: String,
    youtubeLink: String,
    isMuted: Number,
    playingState: String,
-   //eventTime: Date,
    eventTime: String,
    videoType: String,
    autoplay: Number,
    elapseTime: Number,
    volume: String,
    startAt: Number,
-   //startTime: Date,
-   startTime: Date,
+   startTime: String,
    hashedId: String,
+   sendStatus: {type: String, default: 0},
    videoType: String
   });
 
--- routes/autoplays.js
+++ routes/autoplays.js
@@ -7,15 +7,18 @@ var Autoplay = mongoose.model('Autoplay');
 var schedule = require('node-schedule');
 
 var io = require('socket.io-client');
+var socket = require('socket.io-client');
 
 router.get('/list', function(req, res, next) {
 
-    Autoplay.find({}, function(err,docs){
+    Autoplay.find({}).lean().exec(function(err,docs){
       if (err){
         res.end('Error');
         return next();
       }
-      res.json(docs);
+      console.log("=======",docs)
+      console.log("=======",JSON.stringify(docs))
+      res.end(JSON.stringify(docs));
     });
 
 });
@@ -56,14 +59,29 @@ router.post('/create', function(req, res, next) {
   var autoplay = new Autoplay(req.body)
   autoplay.save(function(err, autoplay){
     if (err){
-      //res.end('error');
       res.json(err);
       return next();
     } else {
+
       var name = autoplay.conferenceId 
       console.log("=================", autoplay.startTime);
       var job = schedule.scheduleJob(name, autoplay.startTime, function(){
-        console.log('The world is going to end today.');
+
+        var connection = socket.connect(autoplay.serverUrl, {
+          secure: true,
+          query:  autoplay.keys
+        });
+
+        socket.on('connect', function(){
+          console("-----------------------------")
+          socket.emit('videoShare:load', JSON.stringify(autoplay))
+          Autoplay.update({_id: autoplay._id},{$set:{sendStatus: 1}},function(err){
+           if (err){
+             console.log("===========", err);
+           }
+          });
+        });
+
       });
       res.json({status: 1});
     }
