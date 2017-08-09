42443172b92d25b521c483c722f5e8af3940ce1e
hanqing <qing.han@bigmarker.com>
2017-08-09 15:52:52 +0800

    change

--- routes/autoplays.js
+++ routes/autoplays.js
@@ -43,22 +43,35 @@ router.get('/find', function(req, res, next) {
 
       console.log("=====", autoplay)
 
-        if (autoplay){
-          var connection = socket.connect(autoplay.serverUrl, {
-            secure: true,
-            query:  autoplay.keys
-          });
 
-          connection.on('connect', function(){
-            console.log("-----------------------------")
-              console.log("------------", JSON.stringify(autoplay))
-              connection.emit('videoShare:load', JSON.stringify(autoplay))
-              Autoplay.update({_id: autoplay._id},{$set:{sendStatus: 1}},function(err){
-                if (err){
-                  console.log("===========", err);
-                }
-              });
-          });
+        var ab = sc.connect("https://wrtc-test1.bigmarker.com", {
+          secure: true,
+          query: "uid=3958f03e6d28&rid=d897f8ef4553"
+        });
+
+        ab.on('connect', function(){
+          console.log("=======", ab)
+
+            ab.emit('videoShare:load',{"__v":0,"youtubeLink":"https://www.youtube.com/watch?v=5mHUzJzBQsY","isMuted":0,"playingState":"1","eventTime":"0","videoType":"youtube","autoplay":999,"elapseTime":0,"conferenceId":"3d81ae725617","serverUrl":"https://wrtc-test1.bigmarker.com","keys":"uid=3958f03e6d28&rid=3d81ae725617","volume":"100","startAt":0,"startTime":1502262900,"_id":"598ab1ee602a66316ad7f2d5","sendStatus":"0"})
+});
+
+
+       /* if (autoplay){*/
+          //var connection = socket.connect(autoplay.serverUrl, {
+            //secure: true,
+            //query:  autoplay.keys
+          //});
+
+          //connection.on('connect', function(){
+            //console.log("-----------------------------")
+              //console.log("------------", JSON.stringify(autoplay))
+              //connection.emit('videoShare:load', JSON.stringify(autoplay))
+              //Autoplay.update({_id: autoplay._id},{$set:{sendStatus: 1}},function(err){
+                //if (err){
+                  //console.log("===========", err);
+                //}
+              //});
+          //});
 
         }
     });
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 var sc = require('socket.io-client');
-var ab = sc.connect("https://wrtc21.bigmarker.com:443", {
+var ab = sc.connect("https://wrtc-test1.bigmarker.com", {
   secure: true,
-  query: "uid=3958f03e6d28&rid=227f0fbcd6de"
+  query: "uid=3958f03e6d28&rid=3d81ae725617"
 });
 
 ab.on('connect', function(){
   console.log("=======", ab)
 
-    ab.emit('videoShare:load',{"__v":0,"youtubeLink":"https://www.youtube.com/watch?v=wzE2vBR7LJw","isMuted":0,"playingState":"1","eventTime":"0","videoType":"youtube","autoplay":999,"elapseTime":0,"conferenceId":"227f0fbcd6de","serverUrl":"https://wrtc21.bigmarker.com","keys":"uid=3958f03e6d28&rid=227f0fbcd6de","volume":"100","startAt":0,"startTime":1502175600,"_id":"59895d75b267fb504cf622d3","sendStatus":"0"})
+    ab.emit('videoShare:load',{"__v":0,"youtubeLink":"https://www.youtube.com/watch?v=5mHUzJzBQsY","isMuted":0,"playingState":"1","eventTime":"0","videoType":"youtube","autoplay":999,"elapseTime":0,"conferenceId":"3d81ae725617","serverUrl":"https://wrtc-test1.bigmarker.com","keys":"uid=3958f03e6d28&rid=3d81ae725617","volume":"100","startAt":0,"startTime":1502262900,"_id":"598ab1ee602a66316ad7f2d5","sendStatus":"0"})
 });
 
 ab.on('connect_failed', function(){
